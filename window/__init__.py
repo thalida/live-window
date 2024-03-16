c7f4601e5f75d7feed49d90a284f56c17c83ecd3
Thalida Noel <hello@thalida.com>
2024-03-16 07:50:30 +0000

    Use browser font

--- window/__init__.py
+++ window/__init__.py
@@ -74,31 +74,14 @@ def create_window_svg(
     return f"""
     <svg width="444" height="528" viewBox="0 0 444 528" fill="none" xmlns="http://www.w3.org/2000/svg">
         <style>
-            @font-face {{
-                font-family: 'Barriecito';
-                font-style: normal;
-                font-weight: 400;
-                font-display: swap;
-                src: url(https://fonts.gstatic.com/s/barriecito/v17/WWXXlj-CbBOSLY2QTuY_GdIoYibNwMQ.woff2) format('woff2');
-                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-            }}
-
-            @font-face {{
-                font-family: 'Days One';
-                font-style: normal;
-                font-weight: 400;
-                font-display: swap;
-                src: url(https://fonts.gstatic.com/s/daysone/v18/mem9YaCnxnKRiYZOCIYScrg0V8Bs.woff2) format('woff2');
-                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-            }}
-
             .location-text {{
-                font-family: 'Barriecito', sans-serif;
+                font-family: Trebuchet MS, sans-serif;
+                font-weight: 700;
                 font-size: 24px;
             }}
 
             .currently-text {{
-                font-family: 'Days One', sans-serif;
+                font-family: Trebuchet MS, sans-serif;
                 font-size: 20px;
             }}
         </style>
